6a927d3b593ec3a88fddc0c0322722dfa528346e
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-05-26 10:39:52 -0600

    Hasta 204 - Adding a watcher

--- clase/gulpfile.js
+++ clase/gulpfile.js
@@ -1,16 +1,22 @@
-const { src, dest } = require('gulp')
+const { src, dest, watch } = require('gulp')
 // Have gulp-sass and sass in the same constant
 const sass = require('gulp-sass')(require('sass'))
+
+const MAIN_SASS_ROUTE = 'src/scss/app.scss'
 
 function css(done) {
   // Compile SASS
   // Step 1: Identify file
-  src('src/scss/app.scss')
+  src(MAIN_SASS_ROUTE)
     .pipe(sass()) // Step 2: Compile
     .pipe(dest('build/css')) // Step 3: Save the .css
   
     done()
-
+}
+// Watcher to compile from sass to css
+function dev() {
+  watch(MAIN_SASS_ROUTE, css)
 }
 
 exports.css = css
+exports.dev = dev
