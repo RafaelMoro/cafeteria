ea1a3d3fd8015f512a06e0106934d88d6977473b
Rafael Moro <rafael.moro.galindo@gmail.com>
2024-05-26 10:49:25 -0600

    Hasta clase 206 Add postcss, autoprefixer and gulp-postcss Admit browsers last 1 versions and almost all browsers add post css into css gulp fn

--- clase/gulpfile.js
+++ clase/gulpfile.js
@@ -1,6 +1,8 @@
 const { src, dest, watch } = require('gulp')
 // Have gulp-sass and sass in the same constant
 const sass = require('gulp-sass')(require('sass'))
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
 const MAIN_SASS_ROUTE = 'src/scss/app.scss'
 
@@ -9,6 +11,7 @@ function css(done) {
   // Step 1: Identify file
   src(MAIN_SASS_ROUTE)
     .pipe(sass({ outputStyle: 'compressed' })) // Step 2: Compile
+    .pipe(postcss([autoprefixer()])) // Step 2.1 
     .pipe(dest('build/css')) // Step 3: Save the .css
   
     done()
